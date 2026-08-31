c9338dd5887757470432eeed21761bafa320ec42
pal3241 <kurniadifahri2@gmail.com>
2026-08-31 15:55:09 +0700

    refactor: generalize resource reservation errors

--- src/navigation/movement-policy-factory.js
+++ src/navigation/movement-policy-factory.js
@@ -2,7 +2,7 @@ import { NavigationError } from './navigation-error.js';
 
 export function createMovementPolicy({ policy, lease }) {
   const scaffoldItems = lease?.item ? [lease.item] : [];
-  if (policy.allowPlace && policy.allowScaffolding && !lease) throw new NavigationError('SCAFFOLD_RESERVATION_FAILED', 'Scaffolding requires an active resource lease', {});
+  if (policy.allowPlace && policy.allowScaffolding && !lease) throw new NavigationError('RESOURCE_RESERVATION_FAILED', 'Scaffolding requires an active resource reservation', {});
   return Object.freeze({
     allow1by1towers: Boolean(policy.allowPlace && policy.allowTower && lease),
     allowBridge: Boolean(policy.allowPlace && policy.allowBridge && lease),
